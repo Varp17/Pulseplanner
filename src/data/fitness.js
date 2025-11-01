2d077a27aa9debf3641a2d3cd2d63bb7cc9d6d47
Varp17 <varunpatil0217@gmail.com>
2025-11-01 16:07:00 +0530

    yes

---
+++ src/data/fitness.js
@@ -0,0 +1,108 @@
+const fitness = [
+  {
+    id: 1,
+    name: "Full Body Burn",
+    image:
+      "https://cdn.pixabay.com/photo/2016/11/18/15/32/fitness-1839088_1280.jpg",
+    exercises: [
+      { name: "Jumping Jacks", duration: "30s" },
+      { name: "Push Ups", duration: "30s" },
+      { name: "Squats", duration: "40s" },
+      { name: "Plank", duration: "45s" },
+      { name: "Mountain Climbers", duration: "30s" },
+    ],
+  },
+  {
+    id: 2,
+    name: "Upper Body Power",
+    image:
+      "https://cdn.pixabay.com/photo/2016/03/27/19/40/man-1282232_1280.jpg",
+    exercises: [
+      { name: "Push Ups", duration: "40s" },
+      { name: "Tricep Dips", duration: "30s" },
+      { name: "Shoulder Taps", duration: "30s" },
+      { name: "Plank Up Downs", duration: "30s" },
+      { name: "Arm Circles", duration: "40s" },
+    ],
+  },
+  {
+    id: 3,
+    name: "Leg Day Extreme",
+    image:
+      "https://cdn.pixabay.com/photo/2017/08/10/03/49/people-2618562_1280.jpg",
+    exercises: [
+      { name: "Bodyweight Squats", duration: "45s" },
+      { name: "Jump Lunges", duration: "30s" },
+      { name: "Wall Sit", duration: "40s" },
+      { name: "Calf Raises", duration: "30s" },
+      { name: "Sumo Squats", duration: "45s" },
+    ],
+  },
+  {
+    id: 4,
+    name: "Core Crusher",
+    image:
+      "https://cdn.pixabay.com/photo/2017/08/07/15/53/people-2604149_1280.jpg",
+    exercises: [
+      { name: "Crunches", duration: "30s" },
+      { name: "Leg Raises", duration: "30s" },
+      { name: "Plank", duration: "40s" },
+      { name: "Bicycle Kicks", duration: "30s" },
+      { name: "Russian Twists", duration: "30s" },
+    ],
+  },
+  {
+    id: 5,
+    name: "HIIT Fat Burner",
+    image:
+      "https://cdn.pixabay.com/photo/2016/11/18/15/32/fitness-1839087_1280.jpg",
+    exercises: [
+      { name: "High Knees", duration: "30s" },
+      { name: "Burpees", duration: "30s" },
+      { name: "Jump Squats", duration: "30s" },
+      { name: "Push Ups", duration: "30s" },
+      { name: "Plank", duration: "45s" },
+    ],
+  },
+  {
+    id: 6,
+    name: "Yoga & Stretch",
+    image:
+      "https://cdn.pixabay.com/photo/2016/11/29/13/14/yoga-1869652_1280.jpg",
+    exercises: [
+      { name: "Cat-Cow Stretch", duration: "30s" },
+      { name: "Downward Dog", duration: "30s" },
+      { name: "Child’s Pose", duration: "45s" },
+      { name: "Cobra Pose", duration: "30s" },
+      { name: "Seated Forward Fold", duration: "45s" },
+    ],
+  },
+  {
+    id: 7,
+    name: "Morning Energizer",
+    image:
+      "https://cdn.pixabay.com/photo/2020/05/12/18/10/fitness-5168908_1280.jpg",
+    exercises: [
+      { name: "Arm Swings", duration: "30s" },
+      { name: "Squats", duration: "30s" },
+      { name: "Push Ups", duration: "30s" },
+      { name: "Jumping Jacks", duration: "30s" },
+      { name: "Lunges", duration: "40s" },
+    ],
+  },
+  {
+    id: 8,
+    name: "Night Relax Routine",
+    image:
+      "https://cdn.pixabay.com/photo/2020/06/29/16/49/yoga-5353626_1280.jpg",
+    exercises: [
+      { name: "Neck Rolls", duration: "30s" },
+      { name: "Shoulder Stretch", duration: "30s" },
+      { name: "Hamstring Stretch", duration: "40s" },
+      { name: "Butterfly Stretch", duration: "40s" },
+      { name: "Child’s Pose", duration: "1min" },
+    ],
+  },
+];
+
+export default fitness;
